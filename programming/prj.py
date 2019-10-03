a87f557cee7ad8118c338926055c4f7dd65e1f14
Cameron <cmcgregor15@hswv.co.uk>
2019-10-03 12:33:10 +0100

    Bye mySQL lol

--- programming/prj.py
+++ programming/prj.py
@@ -2,19 +2,6 @@
 import random
 import sys
 import time
-import mysql.connector
-
-#MySQL Database Setup (for login auth)
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="yourusername",
-    passwd="yourpassword",
-    database="loginauthdb"
-)
-
-mycursor = mydb.cursor()
-
-mycursor.execute("CREATE DATABASE loginauthdb")
 
 # Code
 print("Welcome to the Dice Game")
