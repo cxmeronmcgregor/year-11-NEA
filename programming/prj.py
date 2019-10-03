5f22808444bf9f5896f132ec91e264a5f8d75fe8
Cameron <cmcgregor15@hswv.co.uk>
2019-10-03 12:31:57 +0100

    Rework of code

--- programming/prj.py
+++ programming/prj.py
@@ -2,6 +2,22 @@
 import random
 import sys
 import time
+import mysql.connector
+
+#MySQL Database Setup (for login auth)
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="yourusername",
+    passwd="yourpassword",
+    database="loginauthdb"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE DATABASE loginauthdb")
 
 # Code
 print("Welcome to the Dice Game")
+print("------------------------")
+
+time.sleep(3)
