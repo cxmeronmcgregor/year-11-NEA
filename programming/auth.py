70c04abdf0a6edebc854a7c92a5082eaa0b5918a
Cameron <cmcgregor15@hswv.co.uk>
2019-10-09 08:51:40 +0100

    Reworked auth.py

--- programming/auth.py
+++ programming/auth.py
@@ -1,3 +1,23 @@
+## Import
+
 import time
 import sys
-import
+
+#Username and pwd defined in AUTH.md
+authuscounter = 0
+while authuscounter < 3:
+    authinputuser=input("Please input your username: ")
+    if authinputuser != "DesignatedUser":
+        authuscounter = authuscounter + 1
+    else:
+        authpwcounter = 0
+        while authpwcounter < 3:
+            authinputpw=input("Please input your password: ")
+            if authinputpw != "DesignatedPassword":
+                authpwcounter = authpwcounter + 1
+            else:
+                authpwcounter = authpwcounter + 3
+                print("Variable complete.")
+
+else:
+    print("You have been locked out...")
