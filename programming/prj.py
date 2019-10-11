e0994e5988d5e3ae5357b1bff4043a11071ca504
Cameron <cmcgregor15@hswv.co.uk>
2019-10-11 08:17:38 +0100

    Removed old auth.py from prj.py (update)

--- programming/prj.py
+++ programming/prj.py
@@ -8,19 +8,3 @@ print("Welcome to the Dice Game")
 print("------------------------")
 
 time.sleep(3)
-
-authuscounter = 0
-while authuscounter < 3:
-    authinputuser=input("Please input your username: ")
-    if authinputuser != "DesignatedUser":
-        authuscounter = authuscounter + 1
-    else:
-        authpwcounter = 0
-        while authpwcounter < 3:
-            authinputpw=input("Please input your password: ")
-            if authinputpw != "DesignatedPassword":
-                authpwcounter = authpwcounter + 1
-            else:
-                print(authpwcounter)
-else:
-    print("You have been locked out after too many attempts.")
