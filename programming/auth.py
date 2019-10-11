54819b8d2b2a72414ea5f4c1d19c8b684bd7c0ee
Cameron <cmcgregor15@hswv.co.uk>
2019-10-11 08:08:01 +0100

    Recommitted auth.py without changes to prj.py

--- programming/auth.py
+++ programming/auth.py
@@ -6,17 +6,35 @@ import sys
 authuscounter = 0
 while authuscounter < 3:
     authinputuser=input("Please input your username: ")
+    if authinputuser == "":
+        print("No input detected.")
     if authinputuser != "DesignatedUser":
         authuscounter = authuscounter + 1
     else:
         authpwcounter = 0
         while authpwcounter < 3:
             authinputpw=input("Please input your password: ")
+            if authinputpw == "":
+                print("No input detected.")
             if authinputpw != "DesignatedPassword":
                 authpwcounter = authpwcounter + 1
             if authinputpw == "DesignatedPassword":
-                break
-
-
+                print()
 else:
-    print("You have been locked out after too many attempts.")
+    print("You have been locked out after too many username attempts.")
+    sys.exit()
+
+sys.exit()
+
+authpwcounter = 0
+while authpwcounter < 3:
+    authinputpw=input("Please input your password: ")
+    if authinputpw == "":
+        print("No input detected.")
+    if authinputpw != "DesignatedPassword":
+        authpwcounter = authpwcounter + 1
+    if authinputpw == "DesignatedPassword":
+        print()
+else:
+    print("You have been locked out after too many password attempts.")
+    sys.exit()
