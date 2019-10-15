a5fc807ca1e669171b8c7cf1abc58ae3f52f8104
Cameron <cmcgregor15@hswv.co.uk>
2019-10-15 14:01:57 +0100

    Staged changes in auth.py

--- programming/auth.py
+++ programming/auth.py
@@ -8,7 +8,7 @@ while authuscounter < 3:
     authinputuser=input("Please input your username: ")
     if authinputuser == "":
         print("No input detected.")
-    if authinputuser != "DesignatedUser":
+    if authinputuser != "devus":
         authuscounter = authuscounter + 1
     else:
         authpwcounter = 0
@@ -16,20 +16,22 @@ while authuscounter < 3:
             authinputpw=input("Please input your password: ")
             if authinputpw == "":
                 print("No input detected.")
-            if authinputpw != "DesignatedPassword":
+            if authinputpw != "devpw":
                 authpwcounter = authpwcounter + 1
-            if authinputpw == "DesignatedPassword":
-                print()
-else:
+            if authinputpw == "devpw":
+                break
+if authuscounter == 3:
     print("You have been locked out after too many username attempts.")
+
+
 authpwcounter = 0
 while authpwcounter < 3:
     authinputpw=input("Please input your password: ")
     if authinputpw == "":
         print("No input detected.")
-    elif authinputpw != "DesignatedPassword":
+    elif authinputpw != "devpw":
         authpwcounter = authpwcounter + 1
-    elif authinputpw == "DesignatedPassword":
-        print("Success! Logging in...")
-else:
+    elif authinputpw == "devpw":
+        break
+if authpwcounter == 3:
     print("You have been locked out after too many password attempts.")
