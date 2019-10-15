3736ced8d0adb9079ddef82c3f38e16f8b70aef0
Cameron <cmcgregor15@hswv.co.uk>
2019-10-15 14:24:36 +0100

    Add save2file (+ winner)

---
+++ programming/save2file.py
@@ -0,0 +1,10 @@
+#import sys
+#import time
+
+#dicewinner= "player_2"
+
+#f = open ("winner.txt", "w")
+
+#f = open("winner.txt", "a")
+#f.write("The winner of this Dice Game was {}")format.(player_2)
+#f.close()
