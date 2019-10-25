ec6acc15e9832fdbd9841f9980af36ba7572c823
Cameron <cmcgregor15@hswv.co.uk>
2019-10-25 09:30:48 +0100

    Newly staged changes

---
+++ programming/friday-scripts/scrp.py
@@ -0,0 +1,9 @@
+names = ["Akshay","Maxwell","Alfie","Jay"]
+
+find = input("Who do you want to find?")
+
+for i in range(len(names)):
+    print(names[i])
+
+if find == names[i]:
+    print(find,"found")
