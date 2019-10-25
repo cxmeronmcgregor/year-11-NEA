6e074b5fb63cb4d433ed6dba5b11bb58915ec519
Cameron <cmcgregor15@hswv.co.uk>
2019-10-25 11:04:28 +0100

    Fixed it lol

--- programming/prj.py
+++ programming/prj.py
@@ -8,33 +8,17 @@ print("------------------------")
 
 time.sleep(3)
 #Username and pwd defined in AUTH.md
-authuscounter = 0
-while authuscounter < 3:
+counter = 5
+while True:
     authinputuser=input("Please input your username: ")
     if authinputuser == "":
         print("No input detected.")
-    if authinputuser != "DesignatedUser":
-        authuscounter = authuscounter + 1
+    if authinputuser !="DesignatedUser":
+        ()
+    if authinputuser == "DesignatedUser":
+        authinputpw=input("Please input your password: ")
+    if authinputpw == "DesignatedPassword":
+        break
     else:
-        authpwcounter = 0
-        while authpwcounter < 3:
-            authinputpw=input("Please input your password: ")
-            if authinputpw == "":
-                print("No input detected.")
-            if authinputpw != "DesignatedPassword":
-                authpwcounter = authpwcounter + 1
-            if authinputpw == "DesignatedPassword":
-                print()
-else:
-    print("You have been locked out after too many username attempts.")
-authpwcounter = 0
-while authpwcounter < 3:
-    authinputpw=input("Please input your password: ")
-    if authinputpw == "":
-        print("No input detected.")
-    elif authinputpw != "DesignatedPassword":
-        authpwcounter = authpwcounter + 1
-    elif authinputpw == "DesignatedPassword":
-        print("Success! Logging in...")
-else:
-    print("You have been locked out after too many password attempts.")
+        time.sleep(counter)
+        counter=counter*10
