9426c840d5cce8e6762a5ff8c2d84e88964e51ad
Cameron <cmcgregor15@hswv.co.uk>
2019-10-25 09:59:53 +0100

    Stacked list commit

--- programming/friday-scripts/scrp2.py
+++ programming/friday-scripts/scrp2.py
@@ -8,3 +8,12 @@ names=[["Akshay",16],
 print(names)
 print(names[2])
 print(names[2][0])
+
+
+#find = input("Who do you want to find?")
+
+for i in range(len(names)):
+
+    print(names[i])
+
+if find == names[i][0]
