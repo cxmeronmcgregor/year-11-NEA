697015374f9c65db97030657c2de716cda9b0fc1
Cameron <cmcgregor15@hswv.co.uk>
2019-11-05 12:33:02 +0000

    Temporary add for start of dice game

--- programming/prj.py
+++ programming/prj.py
@@ -22,3 +22,19 @@ while True:
 print("Dice Game Starting...")
 
 time.sleep(1)
+
+def dice_roll(): #Make a new subroutine, 'Dice Roll'
+    input("PLAYER 1 | Please enter to roll the dice") #Enter to continue
+    time.sleep(1) #Sleep for 1 second [pause]
+
+    diceus1 = random.randint(1,6) #Creates new variable & assigns random number from 1-6
+    dice2 = random.randint(1,6)
+    total = dice1+dice2 #Creates new varaible, total, adds dice1 and dice2
+
+    print ("") #Formatting
+    print ("Your Dices") #Prints title
+    print ("----------") #Formatting
+    time.sleep(1) #Sleep for 1 second [pause]
+
+    print ("Dice 1:",dice1)
+    print ("Dice 2:",dice2)
