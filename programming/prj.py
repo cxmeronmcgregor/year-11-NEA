5d6990ed7bf88126d1a41673db86ea59716a2559
Cameron <cmcgregor15@hswv.co.uk>
2019-11-19 14:07:15 +0000

    New stage

--- programming/prj.py
+++ programming/prj.py
@@ -3,6 +3,8 @@ import random
 import time
 import sys
 
+                                                                    
+
 # Code
 print("Welcome to the Dice Game")
 print("------------------------")
@@ -23,18 +25,79 @@ print("Dice Game Starting...")
 
 time.sleep(1)
 
-def dice_roll(): #Make a new subroutine, 'Dice Roll'
+def dice_rollp1(): #Make a new subroutine, 'Dice Roll'
     input("PLAYER 1 | Please enter to roll the dice") #Enter to continue
     time.sleep(1) #Sleep for 1 second [pause]
 
-    diceus1 = random.randint(1,6) #Creates new variable & assigns random number from 1-6
+    dice1 = random.randint(1,6) #Creates new variable & assigns random number from 1-6
     dice2 = random.randint(1,6)
     total = dice1+dice2 #Creates new varaible, total, adds dice1 and dice2
 
     print ("") #Formatting
-    print ("Your Dices") #Prints title
+    print ("P1 Dices") #Prints title
     print ("----------") #Formatting
     time.sleep(1) #Sleep for 1 second [pause]
 
     print ("Dice 1:",dice1)
     print ("Dice 2:",dice2)
+    print("Your total is",total)
+
+dice_rollp1()
+
+def dice_rollp2(): #Make a new subroutine, 'Dice Roll'
+    input("PLAYER 2 | Please enter to roll the dice") #Enter to continue
+    time.sleep(1) #Sleep for 1 second [pause]
+
+    dice3 = random.randint(1,6) #Creates new variable & assigns random number from 1-6
+    dice4 = random.randint(1,6)
+    total2 = dice3+dice4 #Creates new varaible, total, adds dice1 and dice2
+
+    print ("") #Formatting
+    print ("P2 Dices") #Prints title
+    print ("----------") #Formatting
+    time.sleep(1) #Sleep for 1 second [pause]
+
+    print ("Dice 3:",dice3)
+    print ("Dice 4:",dice4)
+    print("Your total is",total2)
+
+dice_rollp2()
+
+def dice_rollp1(): #Make a new subroutine, 'Dice Roll'
+    input("PLAYER 1 | Please enter to roll the dice") #Enter to continue
+    time.sleep(1) #Sleep for 1 second [pause]
+
+    dice1 = random.randint(1,6) #Creates new variable & assigns random number from 1-6
+    dice2 = random.randint(1,6)
+    total = dice1+dice2 #Creates new varaible, total, adds dice1 and dice2
+
+    print ("") #Formatting
+    print ("P1 Dices") #Prints title
+    print ("----------") #Formatting
+    time.sleep(1) #Sleep for 1 second [pause]
+
+    print ("Dice 1:",dice1)
+    print ("Dice 2:",dice2)
+    print("Your total is",total)
+
+dice_rollp1()
+
+def dice_rollp2(): #Make a new subroutine, 'Dice Roll'
+    input("PLAYER 2 | Please enter to roll the dice") #Enter to continue
+    time.sleep(1) #Sleep for 1 second [pause]
+
+    dice3 = random.randint(1,6) #Creates new variable & assigns random number from 1-6
+    dice4 = random.randint(1,6)
+    total2 = dice3+dice4 #Creates new varaible, total, adds dice1 and dice2
+
+    print ("") #Formatting
+    print ("P2 Dices") #Prints title
+    print ("----------") #Formatting
+    time.sleep(1) #Sleep for 1 second [pause]
+
+    print ("Dice 3:",dice3)
+    print ("Dice 4:",dice4)
+    print("Your total is",total2)
+
+dice_rollp2()
+
